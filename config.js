ec39b05b4f3fa0ca2b0dd25a95f0c58582993126
Terhemen001 <akpitiemmanuel3@gmail.com>
2025-04-19 09:46:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const config = {
     PAYSTACK_PUBLIC_KEY: 'pk_live_c4db143d371ee8ff7175b5769e42d23a5948ea20', // Test or Live public key
     WHATSAPP_NUMBER: '2349075328722', // WhatsApp number without +
     CURRENCY: 'NGN',
-    AMOUNT: 500 * 100 // 50000 Naira in kobo
+    AMOUNT: 500 * 100 // 49999 Naira in kobo
 };
 Object.freeze(config);
